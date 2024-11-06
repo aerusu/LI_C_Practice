7427cfe9a406bb1e0c8ff84c39e23f4fb03ed772
Micah Li <datguypepe94@gmail.com>
2024-11-05 23:45:50 -0500

    Mightnight work

--- SQ1/SQ1_P1.c
+++ SQ1/SQ1_P1.c
@@ -10,8 +10,19 @@ Things I am learning in Sequence 1:
 */
 #include <stdio.h>
 
+void doubleVar(int*);
 
 int main(){
 
+    int num = 10;
+    int *num_ptr = &num;
+    doubleVar(num_ptr);
+    printf("%d", num);
+
     return 0;
+}
+
+void doubleVar(int *myNum){
+    *myNum *= 2;
+    return;
 }
